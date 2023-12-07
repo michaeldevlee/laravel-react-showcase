6499b8a11a95a4f5e601a2181b1c055f4db574b5
mikelee53 <58196525+mikelee53@users.noreply.github.com>
2023-12-06 20:09:33 -0800

    updated endpoint with prefix groups

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,8 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/customers', function (){
-    return "hello world";
+Route::prefix('/v1')->group(function (){
+    Route:: get("/customers", function(){
+        return "HELLO";
+    });
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
