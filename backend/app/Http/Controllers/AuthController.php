9de476b551379692f62d9664ddb241d18f7743a0
mikelee53 <mikeleevt@gmail.com>
2023-12-07 20:09:55 -0800

    added register route

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -2,19 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserStoreRequest;
+use App\Models\User;
+use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    use HttpResponses;
+
     public function login(){
         return 'this is the login api';
     }
 
-    public function register(){
-        return 'this is the register api';
+    public function register(UserStoreRequest $request){
+        $request->validated($request->all());
+        $user = User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=> Hash::make($request->password)
+        ]);
+        
+        return $this->success([
+            'user'=> $user,
+            'token' => $user->createToken('API token')->plainTextToken
+        ]);
     }
 
     public function logout(){
-        return 'this is the register api';
+        return 'this is the logout api';
     }
 }
